eb57cb4b5232c5e96d818fa2b4a9f6771c57fd66
TsXor <zhang050525@qq.com>
2022-10-08 14:31:15 +0800

    Modularize and add packaging utils under /builder. Add implementation in PyOpenCL to make use of GPU. Deleted example images and (seems-to-be) useless doc.

---
+++ builder/build.bat
@@ -0,0 +1,15 @@
+@echo off
+cd %~dp0
+mkdir wheel_struct >nul
+mkdir wheel_struct\L0_Smoothing >nul
+xcopy /e /y ..\src wheel_struct\L0_Smoothing >nul
+copy /y setup.py wheel_struct\setup.py >nul
+copy /y ..\README.md wheel_struct\README.md >nul
+cd wheel_struct
+python setup.py sdist bdist_wheel
+cd ..
+del /s /q dist >nul
+rmdir /s /q dist >nul
+move /y wheel_struct\dist . >nul
+del /s /q wheel_struct >nul
+rmdir /s /q wheel_struct >nul
